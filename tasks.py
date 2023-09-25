fe77aa64af5a53b5ff56612f3eae5892e68ee3ac
l02 <l8556@outlook.com>
2023-09-25 14:47:36 +0300

    rename open test

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ from tests.desktop_test import DesktopTest
 
 
 @task
-def desktop_test(c, version=None, display=False, config=None, telegram=False, license=None):
+def open_test(c, version=None, display=False, config=None, telegram=False, license=None):
     DesktopTest(
         version=version,
         virtual_display=display,
@@ -22,6 +22,7 @@ def install_desktop(c, version=None, config=None, license=None):
         custom_config=config if config else None,
         license_file_path=license
     )
+
     test.install_package()
     test.check_installed()
     test.check_correct_version()
