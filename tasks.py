dd74d1906b9241dc60ba2baf3ef60ab3be17401c
l02 <l8556@outlook.com>
2023-10-06 19:05:18 +0300

    added update test

--- tasks.py
+++ tasks.py
@@ -5,25 +5,16 @@ from tests.desktop_tests import DesktopTests
 
 
 @task
-def open_test(c, version=None, display=False, config=None, telegram=False, license=None):
+def open_test(c, version=None, update_from=None, display=False, config=None, telegram=False, license=None):
     DesktopTests(
         version=version,
+        old_version=update_from,
         virtual_display=display,
         custom_config=config if config else None,
         telegram=telegram,
         license_file_path=license
     ).open_test()
 
-@task
-def update_test(c, version=None, update_from=None, display=False, config=None, telegram=False, license=None):
-    DesktopTests(
-        version=version,
-        virtual_display=display,
-        custom_config=config if config else None,
-        telegram=telegram,
-        old_version=update_from,
-        license_file_path=license
-    ).update_test()
 
 @task
 def install_desktop(c, version=None, config=None, license=None):
