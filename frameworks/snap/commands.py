390c9e3943580ef922b8d0854d8068381f375ff6
l02 <l8556@outlook.com>
2025-02-07 14:28:57 +0300

    .

--- frameworks/snap/commands.py
+++ frameworks/snap/commands.py
@@ -11,7 +11,7 @@ class SnapCommands:
     def __post_init__(self):
         self.commands: dict = FileUtils.read_json(join(dirname(realpath(__file__)), 'commands.json'))
         self._host = HostInfo()
-        self.host_name = HostInfo().name().lower()
+        self.host_name = self._host.name().lower()
         self.install_commands: list = self._get_install_commands()
         self.version: str = "snap --version"
 
@@ -19,15 +19,12 @@ class SnapCommands:
         return self.commands.get(self._get_os_family(), [])
 
     def _get_os_family(self) -> str:
-        _key = f"{self._host.name()} {self._host.version}"
-        print(f"SnapPackage KEY: {_key}")  # TODO
-
         for os_family, distributions in self.commands['os_family'].items():
             if self.host_name in distributions:
                 return os_family
 
         raise ValueError(
             f"[red]|ERROR| Can't verify os family for download desktop package.\n"
-            f"host name: {self._host.name()}\n"
+            f"host name: {self.host_name}\n"
             f"version: {self._host.version}"
         )
