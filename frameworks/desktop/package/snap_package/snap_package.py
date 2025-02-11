96a27b0a10e16ec8b91c51ca96a1557f54429694
l02 <l8556@outlook.com>
2025-02-11 16:48:49 +0300

    .

--- frameworks/desktop/package/snap_package/snap_package.py
+++ frameworks/desktop/package/snap_package/snap_package.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from os.path import dirname, realpath
+from os.path import dirname, realpath, join
 from subprocess import call
 
 from frameworks.host_control import FileUtils
