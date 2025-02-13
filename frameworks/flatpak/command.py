5ecc2dfc61db5e1fc811d21981cdf8f21f9ae195
l02 <l8556@outlook.com>
2025-02-13 18:19:28 +0300

    .

--- frameworks/flatpak/command.py
+++ frameworks/flatpak/command.py
@@ -28,5 +28,5 @@ class Commands:
 
     def _get_installing_command(self) -> list:
         specific_os = f"{self.host_name} {self._host.version}"
-        print(specific_os)
+        print(f"!!!!!!!!!!!!!!!!!!!!!!!!!{specific_os.upper()}")
         return self.commands.get(specific_os) or self.commands.get(self._get_os_family(), [])
